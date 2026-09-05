152f30e09904600861070a574b2823294c3383da
Aaditya Veer Rana <veerranaaaditya@gmail.com>
2026-09-05 06:14:48 +0530

    Day 1: Q1 - Sum of Two Numbers

--- Day1/Q1.c
+++ Day1/Q1.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
-int main(){
-  int day;
-  printf("enter day");
-  scanf("%d", &day);
-  switch (day){
-    case 1: printf("monday");
-          break;
-    case 2: printf("tuesday");
-          break;
-    case 3: printf("wednesday");
-          break;
-    case 4: printf("thursday");
-         break;
-    case 5: printf("friday");
-        break;
-   case 6: printf("saturday");
-        break;
-   case 7: printf("sunday");
-  }
-  return 0;
+
+int main()
+{
+    int a, b, sum;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a+b;
+    printf("the sum is %d", sum);
+    return 0;
 }
-      
       
       
       
