1516e7fdfb1860a3be48ca91398765ace76d9475
Aaditya Veer Rana <veerranaaaditya@gmail.com>
2026-09-05 07:00:04 +0530

    Day 1: Q2- Arithmetic operations

---
+++ Day1/Q2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum,difference,product;
+    float quotient;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    sum = a+b;
+    difference = a-b;
+    product = a*b;
+    
+    printf ("sum is %d\n", sum);
+    printf("difference is %d\n", difference);
+    printf("product is %d\n", product);
+    
+    if (b != 0){
+        quotient = (float)a/b;
+        printf("quotient is %.2f\n", quotient);
+    }
+    else{
+        printf("cannot divide by zero");
+    }
+    return 0;
+    
+
+
+}
