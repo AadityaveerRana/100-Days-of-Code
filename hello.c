e7ca1e62ce2fcf90e3c7e37e517e6d01a78955cb
Aaditya Veer Rana <veerranaaaditya@gmail.com>
2026-09-26 03:10:32 +0530

    Rename day folders for proper ordering

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int even;
+    printf("enter n");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        i%2 == 0;
+        
+        printf("%d\n", );
+
+    }
+    
+    return 0;
+}
