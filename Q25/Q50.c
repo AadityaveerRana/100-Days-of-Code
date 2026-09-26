b855a6e699d8480afd4433570816347b23a7dc17
Aaditya Veer Rana <veerranaaaditya@gmail.com>
2026-09-27 03:53:02 +0530

    Day 25: Q50 - Decreasing star pattern

---
+++ Q25/Q50.c
@@ -0,0 +1,24 @@
+/*
+Q50: Write a program to print the following pattern:
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+
+        // Print spaces
+        for (int j = 1; j < i; j++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (int j = i; j <= 5; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
