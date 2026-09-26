afb78c89b6c47e893f09f995f495ed404b87f52b
Aaditya Veer Rana <veerranaaaditya@gmail.com>
2026-09-27 03:47:28 +0530

    Day 23: Q45 - Sum of series

---
+++ Day23/Q45.c
@@ -0,0 +1,24 @@
+/*
+Q45: Write a program to find the sum of the series:
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 0.0;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        float numerator = 2 * i;
+        float denominator = 4 * i - 1;
+
+        sum = sum + numerator / denominator;
+    }
+
+    printf("Approximate sum: %.2f", sum);
+
+    return 0;
+}
